2f3e28f02b8decf0703dd2ab7b5c01932e6879ad
pmfoysal <pmfoysal@gmail.com>
2023-03-14 16:02:58 +0600

    Some Changes Added 5

--- src/routes/apis/index.js
+++ src/routes/apis/index.js
@@ -2,5 +2,16 @@ const v1 = require('./v1');
 const api = require('express').Router();
 
 api.use('/v1', v1);
+api.get('/jobs', (req, res) => {
+   res.json({
+      jobs: [
+         {
+            id: 1,
+            name: 'Something',
+            salary: 20000,
+         },
+      ],
+   });
+});
 
 module.exports = api;
